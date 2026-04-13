05aff7948531382082f5772cabd0664111619507
Drexelle Magbual <dref_magbual@nvsu.edu.ph>
2026-04-13 19:27:19 +0800

    Added Hello text in index.html

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,30 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <div>
-    HI, I AM DREXELLE JAZTEEN F. MAGBUAL
-
+    <a href="https://vite.dev" target="_blank">
+      <img src="../src/assets/finalpic.jpg" class="logo" alt="Vite logo"/>
+    </a>
+    
   </div>
-  <HelloWorld msg="CODING + LIFESTYLE" />
+
+  <HelloWorld msg="Drexelle Jazteen F. Magbual" />
+  
+
 </template>
 
 <style scoped>
-
+.logo {
+  height: 10em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+  border-radius: 50%;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 5em #bd0b0baa);
+  
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -11,29 +11,24 @@ const count = ref(0)
 <template>
   <h1>{{ msg }}</h1>
 
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
-  </div>
+ 
 
   <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
+    See more on
+    <a href="https://github.com/DrexellejazteenMagbual" target="_blank"
+      >Github</a
+    >, for more repositories and projects.
   </p>
   <p>
-    Learn more about IDE Support for Vue in the
+    Learn more about me in 
     <a
-      href="https://vuejs.org/guide/scaling-up/tooling.html#ide-support"
+      href="https://www.facebook.com/AkioTakanashi.1.2"
       target="_blank"
-      >Vue Docs Scaling up Guide</a
+      >to learn more about me.</a
     >.
   </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+  <p class="read-the-docs">have a nice day, programmers.</p>
+  
 </template>
 
 <style scoped>
